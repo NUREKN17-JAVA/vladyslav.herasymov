927613f736ae8d586328fdbb6dbe5759dc1bcf83
VladyslavHerasymov <vladyslav.herasymov@nure.ua>
2019-11-02 21:28:27 +0200

    first commit

---
+++ src/main/java/ua/nure/kn/herasymov/usermanagement_me/User.java
@@ -0,0 +1,48 @@
+package ua.nure.kn.herasymov.usermanagement_me;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class User {
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private Date dateOfBirthd;
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Date getDateOfBirthd() {
+		return dateOfBirthd;
+	}
+	public void setDateOfBirthd(Date dateOfBirthd) {
+		this.dateOfBirthd = dateOfBirthd;
+	}
+	public Object getFullName() {
+		return getLastName() + ", " + getFirstName();
+	}
+	public int getAge() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.setTime(new Date());
+		int currentYear = calendar.get(Calendar.YEAR);
+		calendar.setTime(getDateOfBirthd());
+		int year = calendar.get(Calendar.YEAR);
+		return currentYear - year;
+	}
+	
+}
---
+++ src/test/java/ua/nure/kn/herasymov/usermanagement_me/UserTest.java
@@ -0,0 +1,34 @@
+package ua.nure.kn.herasymov.usermanagement_me;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import junit.framework.TestCase;
+import ua.nure.kn.herasymov.usermanagement_me.User;
+
+public class UserTest extends TestCase {
+
+	private User user;
+	private Date dateOfBirthd;
+	
+	protected void setUp() throws Exception {
+		super.setUp();
+		user = new User();
+		
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(1984, Calendar.MAY, 26);
+		dateOfBirthd = calendar.getTime();
+		
+	}
+
+	public void testGetFullName() {
+		user.setFirstName("John");
+		user.setLastName("Doe");
+		assertEquals("Doe, John", user.getFullName());
+	}
+	
+	public void testGetAge() {
+		user.setDateOfBirthd(dateOfBirthd);
+		assertEquals(2019 - 1984, user.getAge());
+	}
+}
